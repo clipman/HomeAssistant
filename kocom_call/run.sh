1a75f30459c4b8db6c9c82baecb01b83aa09e80f
clipman <clipman@naver.com>
2022-03-30 19:49:43 +0900

    Update run.sh

--- kocom_call/run.sh
+++ kocom_call/run.sh
@@ -2,19 +2,19 @@
 
 SHARE_DIR=/share/kocom
 
-if [ ! -f $SHARE_DIR/kocom.py ]; then
+if [ ! -f $SHARE_DIR/kocom_call.py ]; then
 	mkdir $SHARE_DIR
-	mv /kocom.py $SHARE_DIR
-	mv /kocom.conf $SHARE_DIR
+	mv /kocom_call.py $SHARE_DIR
+	mv /kocom_call.conf $SHARE_DIR
 fi
 
-echo "[Info] Run Kocom Wallpad with RS485!"
+echo "[Info] Run Kocom Call with RS485!"
 
 python3 -m pip install pyserial
 python3 -m pip install paho-mqtt
 
 cd $SHARE_DIR
-python3 $SHARE_DIR/kocom.py
+python3 $SHARE_DIR/kocom_call.py
 
 # for dev
 while true; do echo "still live"; sleep 100; done

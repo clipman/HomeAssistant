997d3e44f7f11a0a2501112c073ecaac48e218d7
clipman <clipman@naver.com>
2022-03-31 22:32:25 +0900

    Update run.sh

--- kocom_call/run.sh
+++ kocom_call/run.sh
@@ -2,16 +2,14 @@
 
 SHARE_DIR=/share/kocom
 
-if [ ! -f $SHARE_DIR/kocom_call.py ]; then
+if [ ! -f $SHARE_DIR/kocom_call.conf ]; then
 	mkdir $SHARE_DIR
-	mv /kocom_call.py $SHARE_DIR
 	mv /kocom_call.conf $SHARE_DIR
 fi
 
-echo "[Info] Run Kocom Call with RS485!"
+echo "[Info] Run Kocom Call with RS485..."
 
-cd $SHARE_DIR
-python3 $SHARE_DIR/kocom_call.py
+python3 /kocom_call.py
 
 # for dev
 while true; do echo "still live"; sleep 100; done

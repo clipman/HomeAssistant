91226ec75b0d1324ee6bf9e0ddf81e424954d382
clipman <clipman@naver.com>
2022-04-07 21:24:00 +0900

    Update kocom.py

--- kocom/kocom.py
+++ kocom/kocom.py
@@ -657,7 +657,7 @@ def publish_discovery(dev, sub):
         if logtxt != "" and config.get('Log', 'show_mqtt_publish') == 'True':
             logging.info(logtxt)
     if dev == 'light':
-        for num = 1 to int(config.get('User', 'light_count'))
+        for num in range(1, int(config.get('User', 'light_count'))):
             #ha_topic = 'homeassistant/light/kocom_livingroom_light1/config'
             topic = 'homeassistant/light/kocom_livingroom_light{}/config'.format(num)
             payload = {

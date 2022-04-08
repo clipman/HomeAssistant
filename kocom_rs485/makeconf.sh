1ef2d4dbcb4bab2a47e79665dcc85e882d28788e
clipman <clipman@naver.com>
2022-04-08 13:26:54 +0900

    Update makeconf.sh

--- kocom_rs485/makeconf.sh
+++ kocom_rs485/makeconf.sh
@@ -9,10 +9,6 @@ CONFIG=`cat $CONFIG_FILE`
 
 for i in $(echo $CONFIG | jq -r 'keys_unsorted | .[]')
 do
-  if [ $i == "Advanced" ]
-  then 
-    break
-  fi 
   echo "[$i]" >> $CONFIG_RS485
   echo $CONFIG | jq --arg id "$i" -r '.[$id]|to_entries|map("\(.key)=\(.value|tostring)")|.[]' | sed -e "s/false/False/g" -e "s/true/True/g" >> $CONFIG_RS485
 done

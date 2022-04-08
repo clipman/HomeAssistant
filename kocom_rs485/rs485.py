7e740337ea6519fda7506f56265fb80c8d06c3ef
clipman <clipman@naver.com>
2022-04-08 13:05:19 +0900

    Update rs485.py

--- kocom_rs485/rs485.py
+++ kocom_rs485/rs485.py
@@ -45,43 +45,43 @@ KOCOM_ROOM_THERMOSTAT       = {'00': 'livingroom', '01': 'bedroom', '02': 'room1
 SCAN_INTERVAL = 300         # 월패드의 상태값 조회 간격
 SCANNING_INTERVAL = 0.8     # 상태값 조회 시 패킷전송 간격
 ####################### Start Here by Zooil ###########################
-# option_file = '/data/options.json'
-# if os.path.isfile(option_file):
-#     with open(option_file) as json_file:
-#         json_data = json.load(json_file)
-#         INIT_TEMP = json_data['Advanced']['INIT_TEMP']
-#         SCAN_INTERVAL = json_data['Advanced']['SCAN_INTERVAL']
-#         SCANNING_INTERVAL = json_data['Advanced']['SCANNING_INTERVAL']
-#         DEFAULT_SPEED = json_data['Advanced']['DEFAULT_SPEED']
-#         CONF_LOGLEVEL = json_data['Advanced']['LOGLEVEL']
-#         KOCOM_LIGHT_SIZE = {}
-#         dict_data = json_data['KOCOM_LIGHT_SIZE']
-#         for i in dict_data:
-#             KOCOM_LIGHT_SIZE[i['name']] = i['number']
-#         KOCOM_PLUG_SIZE = {}
-#         dict_data = json_data['KOCOM_PLUG_SIZE']
-#         for i in dict_data:
-#             KOCOM_PLUG_SIZE[i['name']] = i['number']
-#         num = 0
-#         KOCOM_ROOM = {}
-#         list_data = json_data['KOCOM_ROOM']
-#         for i in list_data:
-#             if num < 10:
-#                 num_key = "0%d" % (num)
-#             else:
-#                 num_key = "%d" % (num)
-#             KOCOM_ROOM[num_key] = i
-#             num += 1
-#         num = 0
-#         KOCOM_ROOM_THERMOSTAT = {}
-#         list_data = json_data['KOCOM_ROOM_THERMOSTAT']
-#         for i in list_data:
-#             if num < 10:
-#                 num_key = "0%d" % (num)
-#             else:
-#                 num_key = "%d" % (num)
-#             KOCOM_ROOM_THERMOSTAT[num_key] = i
-#             num += 1
+option_file = '/data/options.json'
+if os.path.isfile(option_file):
+    with open(option_file) as json_file:
+        json_data = json.load(json_file)
+        INIT_TEMP = json_data['Advanced']['INIT_TEMP']
+        SCAN_INTERVAL = json_data['Advanced']['SCAN_INTERVAL']
+        SCANNING_INTERVAL = json_data['Advanced']['SCANNING_INTERVAL']
+        DEFAULT_SPEED = json_data['Advanced']['DEFAULT_SPEED']
+        CONF_LOGLEVEL = json_data['Advanced']['LOGLEVEL']
+        KOCOM_LIGHT_SIZE = {}
+        dict_data = json_data['KOCOM_LIGHT_SIZE']
+        for i in dict_data:
+            KOCOM_LIGHT_SIZE[i['name']] = i['number']
+        KOCOM_PLUG_SIZE = {}
+        dict_data = json_data['KOCOM_PLUG_SIZE']
+        for i in dict_data:
+            KOCOM_PLUG_SIZE[i['name']] = i['number']
+        num = 0
+        KOCOM_ROOM = {}
+        list_data = json_data['KOCOM_ROOM']
+        for i in list_data:
+            if num < 10:
+                num_key = "0%d" % (num)
+            else:
+                num_key = "%d" % (num)
+            KOCOM_ROOM[num_key] = i
+            num += 1
+        num = 0
+        KOCOM_ROOM_THERMOSTAT = {}
+        list_data = json_data['KOCOM_ROOM_THERMOSTAT']
+        for i in list_data:
+            if num < 10:
+                num_key = "0%d" % (num)
+            else:
+                num_key = "%d" % (num)
+            KOCOM_ROOM_THERMOSTAT[num_key] = i
+            num += 1
 ####################### End Here by Zooil ###########################
 ###############################################################################################################
 

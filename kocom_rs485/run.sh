ddcb947d093ea6f54b7a9d6fa904fcf2064ce39c
clipman <clipman@naver.com>
2022-04-08 12:59:00 +0900

    Update run.sh

--- kocom_rs485/run.sh
+++ kocom_rs485/run.sh
@@ -4,10 +4,11 @@ SHARE_DIR=/share/kocom
 
 if [ ! -f $SHARE_DIR/rs485.conf ]; then
 	mkdir $SHARE_DIR
-	mv /rs485.conf $SHARE_DIR
+	mv /rs485.py $SHARE_DIR
 fi
+/makeconf.sh
 
-echo "[Info] Run Kocom Wallpad with RS485..."
+echo "[Info] Run Kocom Wallpad Controller"
 
 cd $SHARE_DIR
 python3 /rs485.py

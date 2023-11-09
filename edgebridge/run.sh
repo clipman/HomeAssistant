6bfcd68c5dd0d406caf5ed084e924fd79a9a2ab6
Let's fly <clipman@naver.com>
2023-11-09 14:32:23 +0900

    Create run.sh

---
+++ edgebridge/run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+SHARE_DIR=/share/edgebridge
+
+if [ ! -f $SHARE_DIR/edgebridge.cfg ]; then
+	mkdir $SHARE_DIR
+	mv /edgebridge.cfg $SHARE_DIR
+fi
+
+echo "[Info] Bridge Server for SmartThings Edge drivers"
+
+cd $SHARE_DIR
+python3 /edgebridge.py
+
+# for dev
+while true; do echo "still live"; sleep 100; done
